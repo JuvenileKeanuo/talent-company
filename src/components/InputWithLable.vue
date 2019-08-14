a710feaea17bc8cba0f2be736ad5971984db775a
Keanuo <juvenilekeanuo@live.com>
2019-08-14 14:45:33 +0800

    first commit

---
+++ src/components/InputWithLable.vue
@@ -0,0 +1,34 @@
+<template>
+	<div style="margin: 1.25rem;">
+		<Input v-model="value" @on-change="emit">
+		<span slot="prepend">{{label}}</span>
+		</Input>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'InputWithLable',
+		props: {
+			label: String,
+			initValue: String
+		},
+		data () {
+			return {
+				value: ''
+			}
+		},
+		mounted() {
+			this.$data.value = this.$props.initValue
+			this.emit()
+		},
+		methods: {
+			emit() {
+				this.$emit('input', this.$data.value)
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+	  <InputWithLable label="姓名" v-model="form[0]"/>
+	  <InputWithLable label="组织机构代码" v-model="form[1]"/>
+	  <Button type="success" @click="test()">成功按钮</Button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import InputWithLable from '@/components/InputWithLable.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+	InputWithLable
+  },
+  data: () => ({
+	  form: {}
+  }),
+  methods: {
+  	test() {
+  		console.log(this.$data.form)
+  	}
+  },
+}
+</script>
