53b3504a686a4b3a6ca928a86be6a46b7f2a835f
Keanuo <juvenilekeanuo@live.com>
2019-08-15 19:21:54 +0800

    push

--- src/store.js
+++ src/store.js
@@ -4,13 +4,35 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+	state: {
+		count: 155,
+		form: {
+			_from_user: null,
+			_confirmed: false,
+			_basic: {
+				'单位名称': null,
+				'统一社会信用代码': null,
+				'所属地域': null,
+				'所属行业': null,
+				'行业分类': null,
+				'单位性质': null,
+				'填报人': null,
+				'联系电话': null,
+				'QQ': null,
+				'微信': null,
+				'电子邮箱': null
+			},
+			_summary: [],
+			_sum_in: [],
+			_sum_out: [],
+			_out_status: [],
+			_need: []
+		}
+	},
+	mutations: {
 
-  },
-  mutations: {
+	},
+	actions: {
 
-  },
-  actions: {
-
-  }
+	}
 })
--- src/views/CompanyInfo.vue
+++ src/views/CompanyInfo.vue
@@ -1,33 +1,65 @@
 <template>
-    <div>
-        <CompanyInfo></CompanyInfo>
-        <div class="button-box">
-            <Button class="button" type="primary" @click="saveHandle">保存</Button>
-        </div>
-    </div>
+	<div>
+		<CompanyInfo v-model="basic" :initObj="initObj"></CompanyInfo>
+		<div class="button-box">
+			<Button class="button" type="primary" @click="saveHandle">保存</Button>
+		</div>
+	</div>
 </template>
 <style scoped>
-    .button-box {
-        text-align: right;
-        padding-right: 100px;
-    }
-    .button {
-        width: 100px;
-    }
+	.button-box {
+		text-align: right;
+		padding-right: 100px;
+	}
+
+	.button {
+		width: 100px;
+	}
 </style>
 <script>
-    import CompanyInfo from '../components/CompanyInfo.vue';
-    export default {
-        components: {
-            CompanyInfo,
-        },
-        methods: {
-            saveHandle(){
-                this.$message({
-                    message: '保存成功',
-                    type: 'success'
-                });
-            }
-        }
-    }
+	import CompanyInfo from '../components/CompanyInfo.vue';
+	export default {
+		components: {
+			CompanyInfo,
+		},
+		data: () => ({
+			basic: {},
+			initObj: {
+				'单位名称': null,
+				'统一社会信用代码': null,
+				'所属地域': null,
+				'所属行业': null,
+				'行业分类': null,
+				'单位性质': null,
+				'填报人': null,
+				'联系电话': null,
+				'QQ': null,
+				'微信': null,
+				'电子邮箱': null
+			}
+		}),
+		methods: {
+			saveHandle() {
+				
+				this.$message({
+					message: '保存成功',
+					type: 'success'
+				});
+			}
+		},
+		created() {
+			console.log('created')
+			console.log(this.$store.state.form._basic)
+			this.$data.initObj = this.$store.state.form._basic
+		},
+		watch: {
+			basic: {
+				handler(val, oldval) {
+					this.$store.state.form._basic = this.$data.basic
+					console.log('moved',this.$store.state.form._basic)
+				},
+				deep: true
+			}
+		}
+	}
 </script>
